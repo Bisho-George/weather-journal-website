850cf9e85b222db71ab0e207d69cf8fea35e6941
bishogeorge689@gmail.com <bishogeorge689@gmail.com>
2022-09-14 22:47:53 +0200

    fixing error in app.js

--- README.md
+++ README.md
@@ -3,8 +3,22 @@
 ## Overview
 This project requires you to create an asynchronous web app that uses Web API and user data to dynamically update the UI. 
 
+## Author's Name:
+    Bishoy George
 ## Instructions
 This will require modifying the `server.js` file and the `website/app.js` file. You can see `index.html` for element references, and once you are finished with the project steps, you can use `style.css` to style your application to customized perfection.
 
 ## Extras
 If you are interested in testing your code as you go, you can use `tests.js` as a template for writing and running some basic tests for your code.
+
+## Instructions on how to run the project:
+You should run the server by writing node server.js in the terminal and open the localhost on port 5000 
+
+## APIS and methods used:
+openweathermap api
+
+ ## Features:
+ get the weather of any country with the zip code with nice GUI
+
+ ## Dependencies: 
+    none
--- website/app.js
+++ website/app.js
@@ -1,66 +1,81 @@
 /* Global Variables */
-const API_KEY = 'f953b433bf8dd4629aa8b532a2a18594';
+const API_KEY = ',&appid=f953b433bf8dd4629aa8b532a2a18594&units=metric';
 const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?zip=';
-const zip = document.getElementById('zip');
-const feelings = document.getElementById('feelings');
 const generate = document.getElementById('generate');
-generate.addEventListener('click', () => {
-    
-    if (zip.value.trim() === '' || feelings.value.trim() === '') {
-        console.log('button is clicked');
-        alert('please enter zip code and your feelings');
-        return;
-    }
-    getWeatherTem(BASE_URL, zip, API_KEY)
-        .then(data => sendData({
-            date: now,
-            temp: data.main.temp,
-            feelings: feelings.value
-        }))
-        .then(() => updateUI())
-});
+const entry = document.querySelector('.entryHolder');
+const server = 'http://localhost:5000/';
 // Create a new date instance dynamically with JS
 const date = new Date();
-const now = `${date.getMonth() + 1}.${date.getDate()}.${date.getFullYear()}`;
+const now = date.toDateString();
+
+const generateData = () => {
+    const zip = document.getElementById('zip');
+    const feelings = document.getElementById('feelings');
+    getWeatherTem(BASE_URL, zip.value, API_KEY).then(data => {
+        if (data) {
+            const {
+                main: { temp },
+                name: city,
+                weather: [{ description }],
+            } = data;
+            const info = {
+                now,
+                city,
+                temp: Math.round(temp),
+                description,
+                feelings,
+            };
+            sendData(server + '/postData', info);
+            updateUI();
+            entry.classList.add('show');
+        }
+    })
+}
+
+
+generate.addEventListener('click', generateData);
 
 // Get Temperature
 
 async function getWeatherTem(baseUrl, zipCode, apiKey) {
-    const getData = await fetch(`${baseUrl}${zipCode}&appid=${apiKey}&unit=metric`);
+    const getData = await fetch(`${baseUrl}${zipCode}${apiKey}`);
     try {
         const data = await getData.json();
         return data;
     } catch (error) {
-        console.log(Error, error);
+        console.log(Error, error.message);
     }
 }
-async function sendData(data = {}) {
-    const postData = await fetch('/postData', {
-        method: 'POST', 
+// Send Data
+async function sendData(url = '', data = {}) {
+    const postData = await fetch(url, {
+        method: 'POST',
         credentials: 'same-origin',
         headers: {
-            'Content-Type': 'application.json'
+            "Content-Type": "application/json",
         },
         body: JSON.stringify(data),
     });
     try {
         const data = await postData.json();
+        console.log(data);
         return data;
     } catch (error) {
-        console.log(Error, error);
+        console.log(Error, error.message);
     }
 }
 
 // Updating UI
 
 async function updateUI() {
-    const getData = await fetch('/postData');
+    const getData = await fetch(server + '/getData');
     try {
         const data = await getData.json();
-        document.getElementById('date').innerHTML = data.date;
-        document.getElementById('temp').innerHTML = data.temp;
+        document.getElementById('date').innerHTML = data.now;
+        document.getElementById('temp').innerHTML = data.temp + '&degC';
         document.getElementById('content').innerHTML = data.feelings;
+
     } catch (error) {
-        console.log(Error, error);
+        console.log(Error, error.message);
     }
 }
